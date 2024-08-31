eddb6ebfbbe41022c665de36af2f44dcd85b4b24
Dimas <63927493+Yarigore@users.noreply.github.com>
2024-09-01 01:00:14 +0200

    Se ha implementado música de ascensor

--- app/src/main/java/com/example/herraminetas/Musica.java
+++ app/src/main/java/com/example/herraminetas/Musica.java
@@ -1,64 +1,61 @@
 package com.example.herraminetas;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Musica#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class Musica extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    public boolean encendida;
+    private ImageView botonMusica;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public Musica() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Musica.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Musica newInstance(String param1, String param2) {
-        Musica fragment = new Musica();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_musica, container, false);
+        View view = inflater.inflate(R.layout.fragment_musica, container, false);
+
+        botonMusica = (ImageView) view.findViewById(R.id.musica);
+
+        botonMusica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (encendida) apagaMusica();
+                else enciendeMusica();
+                encendida = !encendida;
+            }
+        });
+        return view;
+    }
+
+    public void enciendeMusica(){
+        botonMusica.setBackgroundColor(Color.YELLOW);
+        Intent miReproductor = new Intent(getActivity(), ServicioMusica.class);
+        getActivity().startService(miReproductor);
+    }
+
+    public void apagaMusica(){
+        botonMusica.setBackgroundColor(Color.TRANSPARENT);
+        Intent miReproductor = new Intent(getActivity(), ServicioMusica.class);
+        getActivity().stopService(miReproductor);
     }
 }
--- app/src/main/java/com/example/herraminetas/ServicioMusica.java
+++ app/src/main/java/com/example/herraminetas/ServicioMusica.java
@@ -1,4 +1,41 @@
 package com.example.herraminetas;
 
-public class ServicioMusica {
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+
+import androidx.annotation.Nullable;
+
+public class ServicioMusica extends Service {
+
+    MediaPlayer mediaPlayer;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mediaPlayer = MediaPlayer.create(this, R.raw.musica_ascensor);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.setVolume(100, 100);
+    }
+
+    public int onStartCommand(Intent intent, int flags, int starId){
+        mediaPlayer.start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (mediaPlayer.isPlaying()) mediaPlayer.stop();
+        mediaPlayer.release();
+        mediaPlayer = null;
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
 }
